2c42e06fabeae16278b0be3f7a9c1bb63efe258f
ACH. LUTFIYADI <94595671+2018020100049@users.noreply.github.com>
2021-11-20 22:00:50 +0700

    firts commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/new">GitHub</a>
+                    <a href="https://github.com/2018020100049/mebel">GitHub</a>
                 </div>
             </div>
         </div>
